df6243673fb64ce986dcbc8ae11e1466f9e24800
Victor Alvarado <alvaradoguzman900@gmail.com>
2023-06-13 20:44:41 -0600

    Estado usuario

--- app/Console/Commands/CreateUser.php
+++ app/Console/Commands/CreateUser.php
@@ -31,12 +31,14 @@ class CreateUser extends Command
         $email = 'alvarado.test.sv@gmail.com';
         $password = 'Admin123!';
         $role = 'administrador';
+        $estado = 'a';
 
         $user = new User();
         $user->name = $name;
         $user->email = $email;
         $user->password = Hash::make($password);
         $user->role = $role;
+        $user->estado = $estado;
         $user->save();
 
         $this->info('Usuario creado!');
